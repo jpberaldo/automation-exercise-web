000309efdfbd4a64f1bbfed5bf40a7390bd4a1c9
Joao Pedro Beraldo <jpberaldo98@hotmail.com>
2024-06-07 16:39:32 -0300

    Refatoração de código e implementação do design pattern Page Factory na classe ContinuarOuAdicionarProdutosPage

--- src/test/java/paginas/ContinuarOuAdicionarProdutosPage.java
+++ src/test/java/paginas/ContinuarOuAdicionarProdutosPage.java
@@ -1,23 +1,33 @@
 package paginas;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+@SuppressWarnings({"UnusedReturnValue", "unused", "FieldMayBeFinal"})
 public class ContinuarOuAdicionarProdutosPage {
 
     private WebDriver browser;
 
+    @FindBy(xpath = "//button[@class='btn btn-success close-modal btn-block']")
+    private WebElement botaoContinuarParaAdicionarMaisProdutos;
+
+    @FindBy(linkText = "View Cart")
+    private WebElement botaoContinuarParaCarrinho;
+
     public ContinuarOuAdicionarProdutosPage(WebDriver browser) {
         this.browser = browser;
+        PageFactory.initElements(this.browser, this);
     }
 
     public ProductsPage selecionarBotaoContinuarParaAdicionarMaisProdutos() {
-        browser.findElement(By.xpath("//button[@class='btn btn-success close-modal btn-block']")).click();
+        botaoContinuarParaAdicionarMaisProdutos.click();
         return new ProductsPage(browser);
     }
 
     public CartPage selecionarContinuarParaCarrinho() {
-        browser.findElement(By.linkText("View Cart")).click();
+        botaoContinuarParaCarrinho.click();
         return new CartPage(browser);
     }
 

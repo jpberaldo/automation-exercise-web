5e022bdd31f66d15a1af6b9123c8554062f8bce7
Joao Pedro Beraldo <jpberaldo98@hotmail.com>
2024-06-07 16:50:51 -0300

    Refatoração de código e implementação do design pattern Page Factory na classe ContaCriadaPage

--- src/test/java/paginas/ContaCriadaPage.java
+++ src/test/java/paginas/ContaCriadaPage.java
@@ -1,18 +1,25 @@
 package paginas;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+@SuppressWarnings({"UnusedReturnValue", "unused", "FieldMayBeFinal"})
 public class ContaCriadaPage {
 
     private WebDriver browser;
 
+    @FindBy(css = "a[data-qa='continue-button']")
+    private WebElement botaoContinuar;
+
     public ContaCriadaPage(WebDriver browser) {
         this.browser = browser;
+        PageFactory.initElements(this.browser, this);
     }
 
     public InicialLogonPage clicarNoBotaoContinuar() {
-        browser.findElement(By.cssSelector("a[data-qa='continue-button']")).click();
+        botaoContinuar.click();
         return new InicialLogonPage(browser);
     }
 

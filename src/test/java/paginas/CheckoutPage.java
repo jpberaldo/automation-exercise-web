200ba0d0c181a7eb6904814a8da267c2f3bec66e
Joao Pedro Beraldo <jpberaldo98@hotmail.com>
2024-06-08 13:32:43 -0300

    Refatoração de código e implementação do design pattern Page Factory na classe CheckoutPage

--- src/test/java/paginas/CheckoutPage.java
+++ src/test/java/paginas/CheckoutPage.java
@@ -1,28 +1,39 @@
 package paginas;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+
+@SuppressWarnings({"UnusedReturnValue", "unused", "FieldMayBeFinal"})
 public class CheckoutPage {
 
     private WebDriver browser;
 
+    @FindBy(css = "textarea[name='message']")
+    private WebElement campoComentario;
+
+    @FindBy(linkText = "Place Order")
+    private WebElement botaoPlaceOrder;
+
     public CheckoutPage(WebDriver browser) {
         this.browser = browser;
+        PageFactory.initElements(this.browser, this);
     }
 
     public CheckoutPage preencherComentario(String msg) {
         JavascriptExecutor jse = (JavascriptExecutor) browser;
         jse.executeScript("window.scrollBy(0,750);");
-        browser.findElement(By.cssSelector("textarea[name='message']")).sendKeys();
+        campoComentario.sendKeys();
         return this;
     }
 
     public PaymentPage selecionarBotaoPlaceOrder() {
         JavascriptExecutor jse = (JavascriptExecutor) browser;
         jse.executeScript("window.scrollBy(0,750);");
-        browser.findElement(By.linkText("Place Order")).click();
+        botaoPlaceOrder.click();
         return new PaymentPage(browser);
     }
 

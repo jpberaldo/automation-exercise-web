d7530bcecca9049c5addaceac06df915a39ec95b
Joao Pedro Beraldo <jpberaldo98@hotmail.com>
2024-06-11 22:21:57 -0300

    Refatoração de código e implementação do design pattern Page Factory na classe BrandPage

--- src/test/java/paginas/BrandPage.java
+++ src/test/java/paginas/BrandPage.java
@@ -1,23 +1,33 @@
 package paginas;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+@SuppressWarnings({"UnusedReturnValue", "unused", "FieldMayBeFinal", "FieldCanBeLocal"})
 public class BrandPage {
 
     private WebDriver browser;
 
+    @FindBy(css = "a[href='/brand_products/Polo']")
+    private WebElement produtoPolo;
+
+    @FindBy(css = "a[href='/brand_products/H&M']")
+    private WebElement produtoHM;
+
     public BrandPage(WebDriver browser) {
         this.browser = browser;
+        PageFactory.initElements(this.browser, this);
     }
 
     public BrandPage selecionarNovaBrand(int opcao) {
 
         if (opcao == 1) {
-            browser.findElement(By.cssSelector("a[href='/brand_products/Polo']")).click();
+            produtoPolo.click();
 
         } else if (opcao == 2) {
-            browser.findElement(By.cssSelector("a[href='/brand_products/H&M']")).click();
+            produtoHM.click();
 
         }
         return this;
